2cd1996db62f8f73a19ecd8e47ad16520a9ddbfe
Wyatt <Teck1015@gmail.com>
2017-05-18 14:02:29 -0700

    defined the client socket and computername
    The client socket has been defined with connection the the server socket and sending of the ComputerName received via 'GetComputerNameEx', this call may need to be modified.
    
    Added error checking for Socket Creation.

--- Upload.cpp
+++ Upload.cpp
@@ -21,11 +21,26 @@ using namespace std;
 TCHAR  infoBuf[INFO_BUFFER_SIZE];
 DWORD  bufCharCount = INFO_BUFFER_SIZE;
 
-// Get and display the name of the computer.
+sockaddr_in PLU;
+
 int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+if (sockfd == INVALID_SOCKET){
+        cerr<<"Socket Initialization: Error creating socket"<<endl;
+        system("pause");
+        WSACleanup();
+        exit(11);
+}
 
+PLU.sin_family = AF_INET;
+PLU.sin_port = htons(80);            //Set to port 80 as it is always open
+PLU.sin_addr.s_addr = inet_addr(""); //IP for the server computer in the quotes.
 
+char buffer[256];
+buffer = GetComputerNameEx(infoBuf, &bufCharCount, null); //This needs checking: 'null' may need to be defined.
+sendto(sockfd, buffer, sizeOf(buffer),0,(struct sockaddr *)&PLU, sizeOf(PLU));
 
 if( !GetComputerName( infoBuf, &bufCharCount ) )
   printError( TEXT("GetComputerName") );
 _tprintf( TEXT("\nComputer name:      %s"), infoBuf );
+
+sockfd.close(); //Close the socket connection
