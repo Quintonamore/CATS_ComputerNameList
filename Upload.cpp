06eeee2a4bdc23a130f84f95dcff23a618eb3e62
Wyatt <Teck1015@gmail.com>
2017-05-18 11:45:32 -0700

    Add files via upload
    Client code for uploading completion of CATS_PM to a server.

---
+++ Upload.cpp
@@ -0,0 +1,23 @@
+#include <Windows.h>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+#define INFO_BUFFER_SIZE 32767
+TCHAR  infoBuf[INFO_BUFFER_SIZE];
+DWORD  bufCharCount = INFO_BUFFER_SIZE;
+
+// Get and display the name of the computer.
+int sockfd;
+
+
+if( !GetComputerName( infoBuf, &bufCharCount ) )
+  printError( TEXT("GetComputerName") );
+_tprintf( TEXT("\nComputer name:      %s"), infoBuf );
